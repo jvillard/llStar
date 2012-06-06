ca20d1aee1b486c0e659da246a381e9787266b71
Jules Villard <jules.villard@ens-lyon.org>
2012-06-07 00:46:35 +0100

    more struct examples

--- examples/structs/structs.c
+++ examples/structs/structs.c
@@ -1,4 +1,4 @@
-// clang -O0 -emit-llvm structs.c -c -o structs.bc
+// clang -O0 -emit-llvm bite.c -c -o bite.bc
 #include <stdlib.h>
 
 struct two {
@@ -6,19 +6,19 @@ struct two {
   int b;
 };
 
-struct ole {
+struct prout {
   int *data;
-  struct ole *next;
+  struct prout *next;
 };
 
 int f(int a) {
-  struct ole *p, s;
+  struct prout *p, s;
   int b;
   struct two *t;
 
   s.data = &b;
 
-  p = malloc(sizeof(struct ole));
+  p = malloc(sizeof(struct prout));
   p->data = malloc(sizeof(int));
   p->next = p;
   *(p->next->data) = a;
@@ -35,6 +35,34 @@ int f(int a) {
   free(t);
   free(p);
   return b;
+}
+
+struct ij {
+  int i;
+  int j;
+};
+
+struct oneint {
+  int i;
+};
+
+void setint(int *i) {
+  *i = 0;
+}
+
+void yay_int(struct ij s) {
+  s.i = 4;
+  setint(&(s.i));
+  s.j = 0;
+}
+
+void setifield(struct oneint *s) {
+  s->i = 0;
+}
+
+void yay_yay_int(struct ij s) {
+  setifield((struct oneint *)&s);
+  s.j = 0;
 }
 
 int main() {
