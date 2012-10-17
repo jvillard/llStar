1256fd9fe82684783828abc400cfce36609bac16
Jules Villard <jules.villard@ens-lyon.org>
2012-10-17 21:39:02 +0100

    simplified away string setters during argument parsing

--- src/lstar_config.ml
+++ src/lstar_config.ml
@@ -3,27 +3,15 @@ let logic_file_name = ref "logic"
 let spec_file_name = ref "specs"
 let absrules_file_name = ref "abs"
 let abductrules_file_name = ref "../../logic/abduct.logic"
-let auto_gen_list_logic = ref false
 
+let auto_gen_list_logic = ref false
 let abduction_flag = ref false
 
-let set_logic_file_name n =
-  logic_file_name := n
-
-let set_spec_file_name n =
-  spec_file_name := n
-
-let set_absrules_file_name n =
-  absrules_file_name := n
-
-let set_program_file_name n =
-  program_file_name := n
-
 let arg_list = Config.args_default @ [
-  ("-f", Arg.String(set_program_file_name), "program file name");
-  ("-l", Arg.String(set_logic_file_name), "logic file name");
-  ("-s", Arg.String(set_spec_file_name), "spec file name");
-  ("-a", Arg.String(set_absrules_file_name), "abstraction rules file name");
+  ("-f", Arg.Set_string(program_file_name), "program file name");
+  ("-l", Arg.Set_string(logic_file_name), "logic file name");
+  ("-s", Arg.Set_string(spec_file_name), "spec file name");
+  ("-a", Arg.Set_string(absrules_file_name), "abstraction rules file name");
   ("-abduct_file", Arg.Set_string(abductrules_file_name), "abduction rules file name");
   ("-abduct", Arg.Set(abduction_flag), "toggles abduction on");
   ("-lists", Arg.Set(auto_gen_list_logic), "toggles automatic list abstraction rules generation");
