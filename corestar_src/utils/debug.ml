3d085881ffd265378538addc5cb27b3d1f8db793
Jules Villard <jules.villard@ens-lyon.org>
2013-01-12 17:27:26 +0000

    full debugging enabled by default in corestar

--- corestar_src/utils/debug.ml
+++ corestar_src/utils/debug.ml
@@ -78,7 +78,7 @@ let log_phase = 1 lsl 3
 let log_prove = 1 lsl 4
 let log_specs = 1 lsl 5
 
-let log_active = log_phase lor log_prove
+let log_active = -1
   (* -1 means all, 0 means one, in general use lor *)
 
 let log x = log_active land x <> 0
