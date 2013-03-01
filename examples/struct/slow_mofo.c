ca3b3ab19d706440d5a55064337101f09018205b
Jules Villard <jules.villard@ens-lyon.org>
2013-03-01 15:55:09 +0000

    added an example

---
+++ examples/struct/slow_mofo.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+struct two {
+  int a;
+  int b;
+};
+
+struct ole {
+  int *data;
+  struct ole *next;
+};
+
+int chew_slowly(int a) {
+  struct ole *p, s;
+  int b;
+  struct two *t;
+
+  s.data = &b;
+
+  p = malloc(sizeof(struct ole));
+  if (!p) return a;
+  p->data = malloc(sizeof(int));
+  if (!(p->data)) {
+    free(p);
+    return a;
+  }
+  /* p->next = p; */ /* this is equivalent to the line below */
+  *((struct ole **)((void *)p+sizeof(int *))) = p;
+  *(p->next->data) = a;
+
+  b = *((*p).data);
+  
+  free(p->data);
+
+
+  t = malloc(sizeof(struct two));
+  if (t) {
+    t->a = 4444;
+    t->b = 4445;
+    free(t);
+  }
+
+  free(p);
+  return b;
+}
